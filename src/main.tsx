41ada571d0a50ce59939a8c05d5d17e5f0d95065
rbqdev <rbq.dev@gmail.com>
2024-04-09 14:00:06 -0300

    config: add msw and config worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globalStyles/reset.css";
+import { mockServiceWorker } from "./config/msw/index.ts";
+
+if (process.env.NODE_ENV === "development") {
+  await mockServiceWorker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
