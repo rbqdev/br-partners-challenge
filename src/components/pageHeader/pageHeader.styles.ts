83656ab6f141c4809c9f235eac7df0a774171c84
rbqdev <rbq.dev@gmail.com>
2024-04-10 10:06:18 -0300

    feat: add pageHeader reusable component

---
+++ src/components/pageHeader/pageHeader.styles.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const Header = styled(Box)(
+  () => `
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `
+);
---
+++ src/components/pageHeader/pageHeader.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+import * as Styled from "./pageHeader.styles";
+
+type PageHeaderProps = {
+  title: string;
+  action?: React.ReactElement;
+};
+
+export const PageHeader = ({ title, action }: PageHeaderProps) => {
+  return (
+    <Styled.Header>
+      <Typography variant="h1" fontSize={32}>
+        {title}
+      </Typography>
+      {action && action}
+    </Styled.Header>
+  );
+};
