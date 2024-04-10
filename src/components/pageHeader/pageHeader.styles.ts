33bccd209403c13d76c07018413f6f7333b91d7b
rbqdev <rbq.dev@gmail.com>
2024-04-10 12:00:27 -0300

    refactor: renaming component file

--- src/components/pageHeader/pageHeader.styles.ts
+++
@@ -1,9 +0,0 @@
-import { Box, styled } from "@mui/material";
-
-export const Header = styled(Box)(
-  () => `
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  `
-);
