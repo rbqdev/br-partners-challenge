a81b11a52fa31da84886ed372e9a58ecf5cd8d9b
rbqdev <rbq.dev@gmail.com>
2024-04-11 14:15:33 -0300

    feat: create CustomersMap extending Map as "db"

--- src/config/msw/mocks/db.ts
+++ src/config/msw/mocks/db.ts
@@ -1,13 +1,34 @@
-import { factory, nullable, primaryKey } from "@mswjs/data";
+import { Customer } from "@/schema";
 
-export const db = factory({
-  customer: {
-    id: primaryKey(String),
-    type: String,
-    name: String,
-    businessName: nullable(String),
-    document: Number,
-    email: String,
-    phone: Number,
-  },
-});
+export const customersStorageKey = "customersStorageKey";
+
+class CustomersMap extends Map<string | readonly string[], Customer> {
+  constructor() {
+    super();
+    const sessionStorageItems = sessionStorage.getItem(customersStorageKey);
+    const sessionStorageItemsParsed = JSON.parse(sessionStorageItems!) || {};
+    for (const key in sessionStorageItemsParsed) {
+      this.set(key, sessionStorageItemsParsed[key]);
+    }
+  }
+
+  set(key: string, value: Customer) {
+    super.set(key, value);
+    sessionStorage.setItem(
+      customersStorageKey,
+      JSON.stringify(Object.fromEntries(this))
+    );
+    return this;
+  }
+
+  delete(key: string) {
+    const result = super.delete(key);
+    sessionStorage.setItem(
+      customersStorageKey,
+      JSON.stringify(Object.fromEntries(this))
+    );
+    return result;
+  }
+}
+
+export const customersMap = new CustomersMap();
