561edfaf27983437a4331f09676cda02085c46c5
rbqdev <rbq.dev@gmail.com>
2024-04-11 14:17:10 -0300

    feat: add useCustomMutation

---
+++ src/hooks/useCustomMutation.ts
@@ -0,0 +1,37 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { baseUrl } from "@/constants";
+
+type UseCustomMutationProps = {
+  onSuccess: () => void;
+};
+
+export const useCustomMutation = <T>({ onSuccess }: UseCustomMutationProps) => {
+  const mutation = useMutation({
+    onSuccess,
+    mutationFn: async ({
+      endpoint,
+      body,
+      method,
+    }: {
+      endpoint: string;
+      method?: string;
+      body?: T;
+    }) => {
+      const response = await fetch(`${baseUrl}${endpoint}`, {
+        headers: body
+          ? { "Content-type": "application/json; charset=UTF-8" }
+          : undefined,
+        method: method ?? "POST",
+        body: body ? JSON.stringify(body) : undefined,
+      });
+      const data = await response.json();
+
+      return data;
+    },
+  });
+
+  return {
+    mutation,
+  };
+};
