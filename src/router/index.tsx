3be846823b318a6418662411ebbd8bdee178be4c
rbqdev <rbq.dev@gmail.com>
2024-04-11 14:19:48 -0300

    feat: add create and edit routers

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { CustomerFormController } from "@/pages/CostumerForm";
 import { CustomersController } from "@/pages/Customers";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <CustomersController />,
+  },
+  {
+    path: "/customer/create",
+    element: <CustomerFormController />,
+  },
+  {
+    path: "/customer/edit/:id",
+    element: <CustomerFormController />,
   },
 ]);
