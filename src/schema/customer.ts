820265bde796b252e31c9c19bb972caa3869a8e2
rbqdev <rbq.dev@gmail.com>
2024-04-11 14:19:29 -0300

    feat: update schema + add conditional validation for tradeName

--- src/schema/customer.ts
+++ src/schema/customer.ts
@@ -5,14 +5,24 @@ export enum CustomerType {
   PJ = "PJ",
 }
 
-export const CustomerSchema = z.object({
-  id: z.string(),
-  type: z.enum([CustomerType.PF, CustomerType.PJ]),
-  name: z.string(),
-  businessName: z.string().optional(),
-  document: z.number(),
-  email: z.string(),
-  phone: z.number(),
-});
+export const CustomerSchema = z
+  .object({
+    id: z.string().uuid().describe("primaryKey").optional(),
+    type: z.enum([CustomerType.PF, CustomerType.PJ]),
+    name: z.string(),
+    tradeName: z.string().optional(),
+    document: z.number(),
+    email: z.string().email("Invalid email address"),
+    phone: z.number(),
+  })
+  .superRefine((val, ctx) => {
+    if (val.type === CustomerType.PJ && !val.tradeName) {
+      ctx.addIssue({
+        path: ["tradeName"],
+        code: z.ZodIssueCode.custom,
+        message: `Trade name is required`,
+      });
+    }
+  });
 
 export type Customer = z.infer<typeof CustomerSchema>;
