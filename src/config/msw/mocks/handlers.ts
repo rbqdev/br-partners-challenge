aaa7609e2e5bef722c57be808739a9562cef0250
rbqdev <rbq.dev@gmail.com>
2024-04-11 14:20:33 -0300

    feat: add some handlers as get, post and delete

--- src/config/msw/mocks/handlers.ts
+++ src/config/msw/mocks/handlers.ts
@@ -1,19 +1,48 @@
 import { http, HttpResponse } from "msw";
 import { v4 as uuid } from "uuid";
 
-import { Customer, CustomerType } from "@/schema";
+import { Customer } from "@/schema";
+
+import { customersMap } from "./db";
 
 export const handlers = [
   http.get("/api/customers", () => {
-    return HttpResponse.json<Customer[]>([
-      {
-        id: uuid(),
-        type: CustomerType.PF,
-        name: "Frozen yoghurt",
-        document: 159,
-        email: "test@gmail.com",
-        phone: 24,
-      },
-    ]);
+    return HttpResponse.json<Customer[]>([...customersMap.values()], {
+      status: 200,
+    });
   }),
+  http.get("/api/customers/:id", ({ params }) => {
+    const { id } = params;
+
+    if (!customersMap.has(id)) {
+      return HttpResponse.error();
+    }
+
+    return HttpResponse.json<Customer>(customersMap.get(id), { status: 200 });
+  }),
+  http.post<never, Customer>("/api/customers/create", async ({ request }) => {
+    const newCustomer = await request.json();
+    const customerGeneratedId = uuid();
+
+    customersMap.set(customerGeneratedId, {
+      ...newCustomer,
+      id: customerGeneratedId,
+    });
+
+    return HttpResponse.json(newCustomer, { status: 201 });
+  }),
+  http.delete<never, Customer>(
+    "/api/customers/delete/:id",
+    async ({ params }) => {
+      const { id } = params;
+
+      const deletedCustomer = customersMap.delete(id);
+
+      if (!deletedCustomer) {
+        return new HttpResponse(null, { status: 404 });
+      }
+
+      return HttpResponse.json("Customer deleted!", { status: 201 });
+    }
+  ),
 ];
