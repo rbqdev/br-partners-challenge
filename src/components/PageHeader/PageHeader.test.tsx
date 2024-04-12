22f5e4c2e43e07ae10ff67363b41235744787860
rbqdev <rbq.dev@gmail.com>
2024-04-12 19:15:48 -0300

    test: PageHeader

--- src/components/PageHeader/PageHeader.test.tsx
+++ src/components/PageHeader/PageHeader.test.tsx
@@ -1,8 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+import { PageHeader } from "./PageHeader";
 
 describe("PageHeader", () => {
-  it("should be true: example", () => {
-    render(<></>);
-    expect(true).toBe(true);
+  const mockTitle = "mockTitle";
+  it("should render title", () => {
+    render(<PageHeader title={mockTitle} />);
+    expect(screen.getByText(mockTitle)).toBeInTheDocument();
+  });
+  it("should render action", () => {
+    const mockAction = <>Action</>;
+    render(<PageHeader title={mockTitle} action={mockAction} />);
+    expect(screen.getByText("Action")).toBeInTheDocument();
   });
 });
