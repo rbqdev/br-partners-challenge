78e39e1c8113f30a05a7cb945d7897a269e91611
rbqdev <rbq.dev@gmail.com>
2024-04-12 16:32:54 -0300

    config: config jest + react-testing-library

---
+++ src/components/PageHeader/PageHeader.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+
+describe("PageHeader", () => {
+  it("should be true: example", () => {
+    render(<></>);
+    expect(true).toBe(true);
+  });
+});
