120d7a5fc340748eebad94a45b849f60861c0c8b
rbqdev <rbq.dev@gmail.com>
2024-04-12 14:50:03 -0300

    refactor: PageLayout component refactors

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,17 +1,63 @@
-import { Stack } from "@mui/material";
+import { CircularProgress, Stack, Typography } from "@mui/material";
+import { useCallback } from "react";
 
 import * as Styled from "./PageLayout.styles";
 
 type PageLayoutProps = {
   headerElement?: React.ReactElement;
   children: React.ReactElement;
+  isLoading?: boolean;
+  isError?: boolean;
+  errorMessage?: string;
+  isEmpty?: boolean;
+  emptyMessage?: string;
 };
 
-export const PageLayout = ({ headerElement, children }: PageLayoutProps) => {
+export const PageLayout = ({
+  headerElement,
+  children,
+  isLoading,
+  isError,
+  errorMessage,
+  isEmpty,
+  emptyMessage,
+}: PageLayoutProps) => {
+  const childrenElement = useCallback(() => {
+    if (isLoading) {
+      return (
+        <Styled.FullBoxWrapper>
+          <CircularProgress />
+        </Styled.FullBoxWrapper>
+      );
+    }
+
+    if (isEmpty) {
+      return (
+        <Styled.FullBoxWrapper>
+          <Typography variant="h2" fontSize={42}>
+            {emptyMessage}
+          </Typography>
+        </Styled.FullBoxWrapper>
+      );
+    }
+
+    if (isError) {
+      return (
+        <Styled.FullBoxWrapper>
+          <Typography variant="h2" fontSize={42}>
+            {errorMessage}
+          </Typography>
+        </Styled.FullBoxWrapper>
+      );
+    }
+
+    return <Styled.Content>{children}</Styled.Content>;
+  }, [children, emptyMessage, errorMessage, isEmpty, isError, isLoading]);
+
   return (
     <Stack gap={4} height="100%">
       {headerElement && headerElement}
-      <Styled.Content>{children}</Styled.Content>
+      {childrenElement()}
     </Stack>
   );
 };
