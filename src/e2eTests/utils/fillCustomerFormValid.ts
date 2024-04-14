a93db734f4f2c5322cf25cfeb38601e290b47abf
rbqdev <rbq.dev@gmail.com>
2024-04-14 15:05:10 -0300

    refactor: refactor fillCustomerFormValid

--- src/e2eTests/utils/fillCustomerFormValid.ts
+++ src/e2eTests/utils/fillCustomerFormValid.ts
@@ -21,60 +21,43 @@ export const fillCustomerFormValid = async ({
   customName?: string;
   shouldSubmit?: boolean;
 }) => {
+  const isCompany = type === CustomerType.PJ;
+
   /** Submit button start disabled */
   if (!isEdit) {
     const disabledButton = page.getByTestId("submit-button");
     await expect(disabledButton).toBeDisabled();
   }
 
-  if (type === CustomerType.PF) {
-    const typeElement = page.getByLabel("Type");
-    await typeElement.click();
-    const typeOption = page.getByRole("option", { name: "Individual" });
-    await typeOption.click();
-    const nameElement = page.getByLabel("Name");
-    await nameElement.fill(customName ?? defaultIndividualValuesMock.name);
-    const documentElement = page.getByLabel("Document");
-    await documentElement.fill(defaultIndividualValuesMock.document);
-    const emailElement = page.getByLabel("Email");
-    await emailElement.fill(defaultIndividualValuesMock.email);
-    const phoneElement = page.getByLabel("Phone");
-    await phoneElement.fill(defaultIndividualValuesMock.phone);
-    const phoneMaskedElement = page.getByLabel("Phone");
-    await expect(phoneMaskedElement).toHaveValue(
-      defaultIndividualValuesMock.phoneMasked
-    );
-    const enabledButton = page.getByTestId("submit-button");
-    await expect(enabledButton).toHaveCSS(
-      "background-color",
-      "rgb(46, 125, 50)"
-    );
-  }
+  const typeElement = page.getByLabel("Type");
+  await typeElement.click();
+  const typeOption = page.getByRole("option", {
+    name: isCompany ? "Company" : "Individual",
+  });
+  await typeOption.click();
 
-  if (type === CustomerType.PJ) {
-    const typeElement = page.getByLabel("Type");
-    typeElement.click();
-    await page.getByText("Company").click();
+  if (isCompany) {
     const companyNameElement = page.getByLabel("Company Name");
     await companyNameElement.fill(customName ?? defaultCompanyValuesMock.name);
     const tradeNameElement = page.getByLabel("Trade name");
     await tradeNameElement.fill(defaultCompanyValuesMock.tradeName);
-    const documentElement = page.getByLabel("Document");
-    await documentElement.fill(defaultCompanyValuesMock.document);
-    const emailElement = page.getByLabel("Email");
-    await emailElement.fill(defaultCompanyValuesMock.email);
-    const phoneElement = page.getByLabel("Phone");
-    await phoneElement.fill(defaultCompanyValuesMock.phone);
-    const phoneMaskedElement = page.getByLabel("Phone");
-    await expect(phoneMaskedElement).toHaveValue(
-      defaultIndividualValuesMock.phoneMasked
-    );
-    const enabledButton = page.getByTestId("submit-button");
-    await expect(enabledButton).toHaveCSS(
-      "background-color",
-      "rgb(46, 125, 50)"
-    );
+  } else {
+    const nameElement = page.getByLabel("Name");
+    await nameElement.fill(customName ?? defaultIndividualValuesMock.name);
   }
+
+  const documentElement = page.getByLabel("Document");
+  await documentElement.fill(defaultIndividualValuesMock.document);
+  const emailElement = page.getByLabel("Email");
+  await emailElement.fill(defaultIndividualValuesMock.email);
+  const phoneElement = page.getByLabel("Phone");
+  await phoneElement.fill(defaultIndividualValuesMock.phone);
+  const phoneMaskedElement = page.getByLabel("Phone");
+  await expect(phoneMaskedElement).toHaveValue(
+    defaultIndividualValuesMock.phoneMasked
+  );
+  const enabledButton = page.getByTestId("submit-button");
+  await expect(enabledButton).toHaveCSS("background-color", "rgb(46, 125, 50)");
 
   if (shouldSubmit) {
     const enabledButton = page.getByTestId("submit-button");
