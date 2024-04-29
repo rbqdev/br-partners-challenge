26265d70e2156a22b76e5422d3ac47ea8baca81d
Robson Braga de Queiroz <rbq.dev@gmail.com>
2024-04-29 09:11:59 -0300

    fix: typo

--- src/utils/simulateApiLatency.test.ts
+++ src/utils/simulateApiLatency.test.ts
@@ -1,6 +1,6 @@
 import { simulateApiLatency } from "./simulateApiLatency";
 
-describe("isPhoneNumberValid", () => {
+describe("simulateApiLatency", () => {
   it("should resolve promise as true after 1 second", async () => {
     await simulateApiLatency().then((data) => {
       expect(data).toBe(true);
